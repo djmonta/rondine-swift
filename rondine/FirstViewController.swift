8a3dffe24a6a57c93f207e34315b824b86791077
monta <djmonta@gmail.com>
2014-09-21 16:08:40 +0900

    Initial Commit

---
+++ rondine/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  rondine
+//
+//  Created by 宮本幸子 on 2014/09/21.
+//  Copyright (c) 2014年 Sachiko Miyamoto. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ rondine/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  rondine
+//
+//  Created by 宮本幸子 on 2014/09/21.
+//  Copyright (c) 2014年 Sachiko Miyamoto. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
